d2edc85ef28e5eaa7fa533abffbe12a191184076
Rahulkag100 <rahulkag100@gmail.com>
2022-12-28 15:22:30 +0530

    feedback app

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -16,7 +16,7 @@ export const FeedbackProvider = ({ children }) => {
 
   // Fetch feedback
   const fetchFeedback = async () => {
-    const response = await fetch(`/feedback?_sort=id&_order=desc`)
+    const response = await fetch(`https://feedbackdatabase.netlify.app/db.json?_sort=id&_order=desc`)
     const data = await response.json()
 
     setFeedback(data)
@@ -25,7 +25,7 @@ export const FeedbackProvider = ({ children }) => {
 
   // Add feedback
   const addFeedback = async (newFeedback) => {
-    const response = await fetch('/feedback', {
+    const response = await fetch('https://feedbackdatabase.netlify.app/db.json', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -41,7 +41,7 @@ export const FeedbackProvider = ({ children }) => {
   // Delete feedback
   const deleteFeedback = async (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
-      await fetch(`/feedback/${id}`, { method: 'DELETE' })
+      await fetch(`https://feedbackdatabase.netlify.app/db.json/${id}`, { method: 'DELETE' })
 
       setFeedback(feedback.filter((item) => item.id !== id))
     }
@@ -49,7 +49,7 @@ export const FeedbackProvider = ({ children }) => {
 
   // Update feedback item
   const updateFeedback = async (id, updItem) => {
-    const response = await fetch(`/feedback/${id}`, {
+    const response = await fetch(`https://feedbackdatabase.netlify.app/db.json/${id}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
